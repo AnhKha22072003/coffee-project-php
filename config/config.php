50cff80374080bb34ced4edd66d3bf646d257902
kha.luong <luong.kha.rcvn2012@gmail.com>
2025-11-05 16:23:06 +0700

    feat:set config db

--- config/config.php
+++ config/config.php
@@ -1,1 +1,20 @@
 <?php
+
+try {
+    //host
+    define('HOST', 'localhost');
+
+    //database name
+    define('DB_NAME', 'coffee-blend222');
+
+    //database username
+    define('DB_USER', 'root');
+
+    //database password
+    define('DB_PASSWORD', '');
+
+    $conn = new PDO('mysql:host=' . HOST . ';port=3307;dbname=' . DB_NAME . "", DB_USER, DB_PASSWORD);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
